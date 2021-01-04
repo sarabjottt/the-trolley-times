56b2dbc050a7bb131a1f36548033f20b06c399a5
Sarabjot Singh <info@sujjon.in>
2021-01-04 20:04:07 +1100

    Some Linting

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "G-Q2G8RV69RB",
+        trackingId: 'G-Q2G8RV69RB',
         head: true,
       },
     },
@@ -40,7 +40,7 @@ module.exports = {
         short_name: `Trolley Times`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#ffff`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
         icon: `src/assets/images/icon.png`, // This path is relative to the root of the site.
       },
